c234bc6acc2fdd31ccf9c1f7a4c33147becbcd6e
moodang-teacher <yssmi@naver.com>
2024-10-02 13:57:16 +0900

    구현 완료

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -2,14 +2,18 @@ $(function () {
   // 대상을 변수에 저장
   const $menu = $(".gnb > li");
   const $submenu = $(".submenu");
+  const duration = 300; // 0.3초
 
   // 메뉴에 마우스가 들어왔을 때
   $menu.on("mouseenter", function () {
-    $submenu.slideDown();
+    // .stop() : 현재 진행 중인 애니메이션 즉시 중지
+    $(this).find($submenu).stop().slideDown(duration);
+    $(this).addClass("on");
   });
 
   // 메뉴에 마우스가 나갔을 때
   $menu.on("mouseleave", function () {
-    $submenu.slideUp();
+    $submenu.stop().slideUp(duration);
+    $menu.removeClass("on");
   });
 });
