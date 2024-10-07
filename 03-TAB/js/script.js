0733c15fce15da81645222aca29f40f850396234
moodang-teacher <yssmi@naver.com>
2024-10-07 12:10:14 +0900

    공통의 동작을 함수로 구현

--- 03-TAB/js/script.js
+++ 03-TAB/js/script.js
@@ -2,15 +2,12 @@ $(function () {
   // 대상을 변수에 저장
   const $tabMenu = $(".tab-menu > li");
   const $tabCon = $(".tab-con-item");
+  const bgColor = ["red", "green", "blue"];
 
   // console.log($tabMenu, $tabCon);
 
   // 처음 세팅
-  $tabMenu.removeClass("on");
-  $tabMenu.eq(0).addClass("on");
-
-  $tabCon.hide();
-  $tabCon.eq(0).show();
+  tabAction(0);
 
   // 탭메뉴를 클릭했을 때
   $tabMenu.on("click", function (e) {
@@ -21,12 +18,21 @@ $(function () {
     const tabIdx = $(this).index();
     console.log(tabIdx);
 
+    tabAction(tabIdx);
+  });
+
+  // 공통의 동작을 함수로 정의
+  function tabAction(index) {
     // 탭메뉴 활성화
     $tabMenu.removeClass("on");
-    $tabMenu.eq(tabIdx).addClass("on");
+    $tabMenu.eq(index).addClass("on");
 
     // 인덱스에 해당하는 $tabCon 보이기
     $tabCon.hide();
-    $tabCon.eq(tabIdx).show();
-  });
+    $tabCon.eq(index).show();
+
+    $("body").css({
+      backgroundColor: bgColor[index],
+    });
+  }
 });
